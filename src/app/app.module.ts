e7d4958429aef200f29c9f9a5e30549e3fab87c0
ChandrasekharReddy <tchandu.soft@gmail.com>
2017-08-08 10:09:33 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+import { Md2Module }  from 'md2';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { OrderbyPipe } from './orderby.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    OrderbyPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MaterialModule,
+    Md2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
